8002ddef54fe37ab26cca32b378bbe3ecff08a6a
Tom Webber <t.c.w.webber@gmail.com>
2024-07-04 19:49:42 +0200

    feat: labels for blogs, and renaming

--- src/app/[locale]/admin/blog/edit/[postId]/editor-actions.tsx
+++ src/app/[locale]/admin/blog/edit/[postId]/editor-actions.tsx
@@ -1,92 +1,87 @@
-import { Button } from "@/components/ui/button";
-import { Op } from "quill/core";
 import { AiOutlineLoading3Quarters } from "react-icons/ai";
 import { FaCheck, FaSave } from "react-icons/fa";
 import { FaPencil } from "react-icons/fa6";
 import { TfiWorld } from "react-icons/tfi";
 import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { renameLocaleBlog } from "@/db/blogs";
 import { useRouter } from "next/navigation";
+import type { Op } from "quill/core";
+import type { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
 
-interface ActionProps {
-    content: Op[];
-    value: string;
-    toBeChanged: boolean;
-    setToBeChanged: (_: boolean) => void;
-    localeBlogId: number;
-    title: string;
-}
-
-export const Actions = ({ toBeChanged, setToBeChanged, content, value, localeBlogId, title }: ActionProps) => {
-    return (
-        <div className="p-6 flex items-center justify-between w-full">
-            <div className="flex items-center space-x-2">
-                <Button variant="outline" onClick={() => {}}>
-                    <span className="flex items-center space-x-2">
-                        <p>Traduire</p>
-                        <TfiWorld />
-                    </span>
-                </Button>
-                <Button
-                    variant="outline"
-                    onClick={() => {
-                        setToBeChanged(true);
+const Rename = ({ title, localeBlogId, router }: { title: string; localeBlogId: number; router: AppRouterInstance }) => (
+    <Dialog>
+        <DialogTrigger asChild>
+            <Button variant="outline">
+                <span className="flex items-center space-x-2">
+                    <p>Renommer</p>
+                    <FaPencil />
+                </span>
+            </Button>
+        </DialogTrigger>
+        <DialogContent>
+            <DialogHeader>
+                <DialogTitle>Modification du titre du post</DialogTitle>
+                <form
+                    className="flex flex-col items-center w-full space-y-10"
+                    onSubmit={e => {
+                        const formData = new FormData(e.target as HTMLFormElement);
+                        const title = formData.get("title") as string;
+                        renameLocaleBlog(localeBlogId, title).finally(() => {
+                            router.refresh();
+                        });
                     }}
                 >
-                    <span className="flex items-center space-x-2">
-                        <p>Sauvergarder</p>
-                        <FaSave />
-                    </span>
-                </Button>
-                <Rename localeBlogId={localeBlogId} title={title} />
-            </div>
-            <div>
-                <OpenSave saving={JSON.stringify(content) !== value} />
-            </div>
-        </div>
-    );
-};
+                    <Label htmlFor="title" className="w-full">
+                        Titre
+                    </Label>
+                    <Input defaultValue={title} className="w-full" type="text" name="title" id="title" />
+                    <Button variant="call2action" type="submit">
+                        Renommer
+                    </Button>
+                </form>
+            </DialogHeader>
+        </DialogContent>
+    </Dialog>
+);
 
-const Rename = ({ title, localeBlogId }: { title: string; localeBlogId: number }) => {
-    const router = useRouter();
-    return (
-        <Dialog>
-            <DialogTrigger asChild>
-                <Button variant="outline">
-                    <span className="flex items-center space-x-2">
-                        <p>Renommer</p>
-                        <FaPencil />
-                    </span>
-                </Button>
-            </DialogTrigger>
-            <DialogContent>
-                <DialogHeader>
-                    <DialogTitle>Modification du titre du post</DialogTitle>
-                    <form
-                        className="flex flex-col items-center w-full space-y-10"
-                        onSubmit={e => {
-                            const formData = new FormData(e.target as HTMLFormElement);
-                            const title = formData.get("title") as string;
-                            renameLocaleBlog(localeBlogId, title).finally(() => {
-                                router.refresh();
-                            });
-                        }}
-                    >
-                        <Label htmlFor="title" className="w-full">
-                            Titre
-                        </Label>
-                        <Input defaultValue={title} className="w-full" type="text" name="title" id="title" />
-                        <Button variant="call2action" type="submit">
-                            Renommer
-                        </Button>
-                    </form>
-                </DialogHeader>
-            </DialogContent>
-        </Dialog>
-    );
-};
+const AddLabel = ({ title, localeBlogId, router }: { title: string; localeBlogId: number; router: AppRouterInstance }) => (
+    <Dialog>
+        <DialogTrigger asChild>
+            <Button variant="outline">
+                <span className="flex items-center space-x-2">
+                    <p>Renommer</p>
+                    <FaPencil />
+                </span>
+            </Button>
+        </DialogTrigger>
+        <DialogContent>
+            <DialogHeader>
+                <DialogTitle>Modification du titre du post</DialogTitle>
+                <form
+                    className="flex flex-col items-center w-full space-y-10"
+                    onSubmit={e => {
+                        const formData = new FormData(e.target as HTMLFormElement);
+                        const title = formData.get("title") as string;
+                        renameLocaleBlog(localeBlogId, title).finally(() => {
+                            router.refresh();
+                        });
+                    }}
+                >
+                    <Label htmlFor="title" className="w-full">
+                        Titre
+                    </Label>
+                    <Input defaultValue={title} className="w-full" type="text" name="title" id="title" />
+                    <Button variant="call2action" type="submit">
+                        Renommer
+                    </Button>
+                </form>
+            </DialogHeader>
+        </DialogContent>
+    </Dialog>
+);
 
 const OpenSave = ({ saving }: { saving: boolean }) => (
     <Dialog>
@@ -121,3 +116,44 @@ const OpenSave = ({ saving }: { saving: boolean }) => (
         </DialogContent>
     </Dialog>
 );
+
+interface ActionProps {
+    content: Op[];
+    value: string;
+    toBeChanged: boolean;
+    setToBeChanged: (_: boolean) => void;
+    localeBlogId: number;
+    title: string;
+}
+
+export const Actions = ({ toBeChanged, setToBeChanged, content, value, localeBlogId, title }: ActionProps) => {
+    const router = useRouter();
+    return (
+        <div className="p-6 flex items-center justify-between w-full">
+            <div className="flex items-center space-x-2">
+                <Button variant="outline" onClick={() => {}}>
+                    <span className="flex items-center space-x-2">
+                        <p>Traduire</p>
+                        <TfiWorld />
+                    </span>
+                </Button>
+                <Button
+                    variant="outline"
+                    onClick={() => {
+                        setToBeChanged(true);
+                    }}
+                >
+                    <span className="flex items-center space-x-2">
+                        <p>Sauvergarder</p>
+                        <FaSave />
+                    </span>
+                </Button>
+                <Rename localeBlogId={localeBlogId} title={title} router={router} />
+                <AddLabel localeBlogId={localeBlogId} title={title} router={router} />
+            </div>
+            <div>
+                <OpenSave saving={JSON.stringify(content) !== value} />
+            </div>
+        </div>
+    );
+};
