9eb486c0c3ddedd6592ad3811681032619956b69
Tom Webber <t.c.w.webber@gmail.com>
2024-09-30 20:31:12 +0200

    ui: offer desktop page

--- src/app/[locale]/(client)/offer/client.tsx
+++ src/app/[locale]/(client)/offer/client.tsx
@@ -15,20 +15,21 @@ export function DesktopTimeline({ locale }: { locale: Locale }) {
     return (
         <>
             <div className="flex flex-col items-center w-fit">
+                <div className="h-[10px] w-[2px] bg-primary" />
                 {t.steps.map(({ title }, i) => (
                     <Fragment key={i}>
                         <Button
                             variant="ghost"
-                            className={cn("border-[3px]  border-primary", selected === i ? "bg-primary hover:bg-primary" : "")}
+                            className={cn("border-[2px] w-full border-primary", selected === i ? "bg-primary hover:bg-primary" : "")}
                             key={i}
                             onClick={() => setSelected(i)}
                         >
                             <p>{title}</p>
                         </Button>
-                        <div className="h-[10px] w-[3px] bg-primary" />
+                        <div className="h-[10px] w-[2px] bg-primary" />
                     </Fragment>
                 ))}
-                <div className="w-0 h-0 border-l-8 border-l-transparent border-r-8 border-r-transparent border-t-[14px] border-t-primary" />
+                <div className="w-0 h-0 border-l-8 border-l-transparent border-r-8 border-r-transparent border-t-[16px] border-t-primary" />
             </div>
             <div className="h-full flex items-center">
                 <p>{t.steps[selected].text}</p>
