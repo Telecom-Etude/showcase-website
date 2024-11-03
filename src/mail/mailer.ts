bf1de33ec7648365305ed888bf0ec3743bce848d
Tom Webber <t.c.w.webber@gmail.com>
2024-11-03 01:00:09 +0100

    fix: mailer: added a plain text version & protect xss

--- src/mail/mailer.ts
+++ src/mail/mailer.ts
@@ -2,7 +2,7 @@
 
 import nodemailer from "nodemailer";
 
-export async function sendEmail(dest_email: string[], subject: string, html: string) {
+export async function sendEmail(dest_email: string[], subject: string, html: string, text: string) {
     if (process.env.DEV_MODE) console.log("Send email");
 
     let transporter = nodemailer.createTransport({
@@ -17,7 +17,8 @@ export async function sendEmail(dest_email: string[], subject: string, html: str
         from: process.env.SMTP_EMAIL,
         to: dest_email,
         subject,
-        html
+        html,
+        text
     };
 
     await transporter.sendMail(mailOptions);
@@ -34,6 +35,10 @@ interface FormProps {
     message: string;
 }
 
+function escapeHtml(unsafe: string | undefined): string | undefined {
+    return unsafe?.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&#039;");
+}
+
 export async function sendForm({ name, email, tel, societe, subject, message }: FormProps, emails: string[]) {
     if (process.env.DEV_MODE) console.log("sending form");
 
@@ -44,13 +49,20 @@ export async function sendForm({ name, email, tel, societe, subject, message }: 
         year: "2-digit"
     })} ${date.toLocaleTimeString("fr-FR", { hour: "2-digit", minute: "2-digit" })}`;
 
+    const safeName = escapeHtml(name);
+    const safeEmail = escapeHtml(email);
+    const safeTel = escapeHtml(tel);
+    const safeSociete = escapeHtml(societe);
+    const safeMessage = escapeHtml(message);
+    const safeSubject = escapeHtml(subject) || safeSociete || safeName;
+
     const html = `        
     <!DOCTYPE html>
     <html lang="en">
         <head>
                 <meta charset="UTF-8" />
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                <title>Document</title>
+                <title>Contact | ${safeSubject}</title>
                 <style>
                     body {
                         padding: 10px;
@@ -78,23 +90,23 @@ export async function sendForm({ name, email, tel, societe, subject, message }: 
             </head>
             <body>
                 <div class="infos">
-                    <H1>${subject || societe || name}</H1>
+                    <H1>${safeSubject}</H1>
                     <table>
                         <tr>
                             <td>Nom</td>
-                            <td>${name}</td>
+                            <td>${safeName}</td>
                         </tr>
                         <tr>
                             <td>Société</td>
-                            <td>${societe}</td>
+                            <td>${safeSociete}</td>
                         </tr>
                         <tr>
                             <td>Mail</td>
-                            <td><a href="mailto:${email}">${email}</td>
+                            <td>${safeEmail}</td>
                         </tr>
                         <tr>
                             <td>Téléphone</td>
-                            <td><a href="tel:${tel}">${tel}</a></td>
+                            <td>${safeTel}</td>
                         </tr>
                         <tr>
                             <td>Date</td>
@@ -104,14 +116,26 @@ export async function sendForm({ name, email, tel, societe, subject, message }: 
                     </table>
                 </div>
                 <H2>Message</H2>
-                <div class="text">${message}</div>
+                <div class="text">${safeMessage}</div>
                 </div>
             </body>
         </html>`;
 
-    const email_subject = `[Formulaire de contact TE][${societe || name}] ${subject}`;
+    const text = `
+Nom:\t${safeName}
+Société:\t${safeSociete}
+Mail:\t${safeEmail}
+Tel:\t${safeTel}    
+Date:\t${formattedDate}
 
-    await sendEmail(emails, email_subject, html);
+Message
+
+${message}
+    `;
+
+    const email_subject = `[Formulaire contact][${societe || name}] ${subject}`;
+
+    await sendEmail(emails, email_subject, html, text);
 
     if (process.env.DEV_MODE) console.log("form sent");
 }
