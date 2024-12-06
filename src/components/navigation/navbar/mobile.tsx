e2f0755821844483a2e2690d998e2df278ca3309
Tom Webber <t.c.w.webber@gmail.com>
2024-12-06 09:19:31 +0100

    add: right click redirects to logo page

--- src/components/navigation/navbar/mobile.tsx
+++ src/components/navigation/navbar/mobile.tsx
@@ -7,19 +7,27 @@ import { useState } from "react";
 import { Menu } from "lucide-react";
 import { Links } from "./links/links";
 import { Locale } from "@/locales/config";
-import { User } from "next-auth";
 import { ExtendedUser } from "@/auth/auth";
 import { getDictionary } from "@/locales/dictionaries";
+import { useRouter } from "next/navigation";
 
 export const MobileNavBar = ({ locale, user }: { locale: Locale; user?: ExtendedUser }) => {
     const [deployed, setDeployed] = useState(false);
     const close = () => setDeployed(false);
     const button_vocab = getDictionary(locale).navigation.button;
+    const router = useRouter();
     return (
         <div>
             <div className="flex justify-between border-b-2 border-accent">
                 <Button asChild variant="ghost" className="p-2" onClick={() => close()} aria-label="menu">
-                    <Link href="/" className="flex justify-start px-0 py-0">
+                    <Link
+                        href="/"
+                        className="flex justify-start px-0 py-0"
+                        onContextMenu={e => {
+                            e.preventDefault();
+                            router.push("https://github.com/telecom-etude/logos");
+                        }}
+                    >
                         <FullLogo />
                     </Link>
                 </Button>
--- src/components/navigation/navbar/navbar.tsx
+++ src/components/navigation/navbar/navbar.tsx
@@ -8,6 +8,7 @@ import { Links } from "./links/links";
 import { useState, useEffect } from "react";
 import { Locale } from "@/locales/config";
 import { ExtendedUser } from "@/auth/auth";
+import { useRouter } from "next/navigation";
 
 export const NavBar = ({ locale, user }: { locale: Locale; user?: ExtendedUser }) => {
     const [isScrolled, setIsScrolled] = useState(false);
@@ -28,12 +29,21 @@ export const NavBar = ({ locale, user }: { locale: Locale; user?: ExtendedUser }
         };
     }, []);
 
+    const router = useRouter();
+
     return (
         <>
             <div className="sticky z-30 top-0 bg-background">
                 <div className="hidden lg:flex justify-between border-b-2 border-accent">
                     <Button asChild variant="ghost" className="rounded-none px-0 py-0">
-                        <Link href="/" className="flex justify-start px-0 py-0">
+                        <Link
+                            href="/"
+                            className="flex justify-start px-0 py-0"
+                            onContextMenu={e => {
+                                e.preventDefault();
+                                router.push("https://github.com/telecom-etude/logos");
+                            }}
+                        >
                             <FullLogo />
                         </Link>
                     </Button>
