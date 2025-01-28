b864a824be26f4e48204eb223f13fab3dc2fd195
Tom Webber <t.c.w.webber@gmail.com>
2025-01-28 15:26:34 +0100

    fix: offer page for dark mode

--- src/app/[locale]/(client)/offer/client.tsx
+++ src/app/[locale]/(client)/offer/client.tsx
@@ -20,24 +20,29 @@ export function DesktopTimeline({ locale }: { locale: Locale }) {
     return (
         <>
             <div className="flex h-full flex-col items-center w-fit">
-                <div className="h-[10px] w-[2px] bg-primary" />
+                <div className="h-[10px] w-[2px] bg-primary dark:bg-destructive" />
                 {t.steps.map(({ title }, i) => (
                     <Fragment key={i}>
                         <Button
                             variant="ghost"
-                            className={cn("border-[2px] w-full border-primary", selected === i ? "bg-primary hover:bg-primary" : "")}
+                            className={cn(
+                                "border-[2px] w-full border-primary dark:border-destructive",
+                                selected === i ? "bg-primary dark:bg-destructive dark:hover:bg-destructive hover:bg-x" : ""
+                            )}
                             key={i}
                             onClick={() => setSelected(i)}
                         >
                             <p>{title}</p>
                         </Button>
-                        <div className="h-[10px] w-[2px] bg-primary" />
+                        <div className="h-[10px] w-[2px] bg-primary dark:bg-destructive" />
                     </Fragment>
                 ))}
-                <div className="w-0 h-0 border-l-8 border-l-transparent border-r-8 border-r-transparent border-t-[16px] border-t-primary" />
+                <div className="w-0 h-0 border-l-8 border-l-transparent border-r-8 border-r-transparent border-t-[16px] border-t-primary dark:border-t-destructive" />
             </div>
-            <div className="h-full flex flex-col items-center space-y-8">
-                <Image src={OFFER_IMAGES[selected]} width={300} height={300} alt="" />
+            <div className="h-full flex flex-col  items-center space-y-4">
+                <div className="p-4 bg-white rounded-xl">
+                    <Image src={OFFER_IMAGES[selected]} width={300} height={300} alt="" />
+                </div>
                 <p className="h-[250px]">{t.steps[selected].text}</p>
             </div>
         </>
