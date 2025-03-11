317bbbf0c196e69f0ed102bf3302cd91364d4bf5
gwendalgrison <gwendal.grison@laposte.net>
2025-03-12 00:07:20 +0100

    used the ! modifier to clean the code

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -6,14 +6,13 @@ import { cn } from "@/lib/utils";
 import Link from "next/link";
 
 const buttonVariantsStyles = {
-    default: "call2action text-primary-foreground hover:bg-primary/90 px-4",
-    destructive: "bg-destructive text-destructive-foreground hover:bg-destructive/90 px-4",
-    outline: "border border-input bg-background hover:bg-accent hover:text-accent-foreground px-4",
-    secondary: "bg-secondary text-secondary-foreground hover:bg-secondary/80 px-4",
-    ghost: "hover:bg-accent hover:text-accent-foreground px-4",
-    link: "text-primary underline-offset-4 hover:underline px-4",
-    linkFooter: "text-primary underline-offset-4 hover:underline",
-    call2action: "font-medium flex flex-col justify-center text-primary-foreground rounded-full call2action min-w-[150px] px-4",
+    default: "call2action text-primary-foreground hover:bg-primary/90",
+    destructive: "bg-destructive text-destructive-foreground hover:bg-destructive/90",
+    outline: "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+    secondary: "bg-secondary text-secondary-foreground hover:bg-secondary/80",
+    ghost: "hover:bg-accent hover:text-accent-foreground",
+    link: "text-primary underline-offset-4 hover:underline",
+    call2action: "font-medium flex flex-col justify-center text-primary-foreground rounded-full call2action min-w-[150px]",
 };
 export const buttonVariants = cva(
     "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
@@ -21,7 +20,7 @@ export const buttonVariants = cva(
         variants: {
             variant: buttonVariantsStyles,
             size: {
-                default: "h-10 py-2",
+                default: "h-10 px-4 py-2",
                 sm: "h-9 rounded-md px-3",
                 lg: "h-11 rounded-md px-8",
                 icon: "h-10 w-10",
