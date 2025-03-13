8f9aac7fc3fff870033f2af149502f0300863f69
Allan Villars <allan.villars@telecom-paris.fr>
2025-03-13 01:10:29 +0100

    Menage du spaghetti code que j'avais ajouté

--- src/components/navigation/navbar/links/locale-switcher.tsx
+++ src/components/navigation/navbar/links/locale-switcher.tsx
@@ -7,21 +7,6 @@ import { nav } from "@/locales/routing";
 import { GB, FR } from "country-flag-icons/react/3x2";
 import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
-
-/*  Tentative de lier le bouton pour changer les langues à une api pour récuperer le slug du post traduit pour la redirection
-    Mais le code marche pas à cause du client side et de la nécessité de await
-const fetchSlug = async (slug: string | null) => {
-    try {
-        const response = await fetch(`/api/get-post-slug?slug=${slug}`);
-        if (!response.ok) throw new Error("Erreur de récupération du slug");
-        return await response.json();
-    } catch (error) {
-        console.error(error);
-        return null;
-    }
-};
-*/
 
 const useLocaledUrl = (locale: Locale) => {
     const oldUrl = usePathname();
@@ -32,8 +17,7 @@ const useLocaledUrl = (locale: Locale) => {
     }
     if (index === split.length) {
         return nav(locale, "/");
-    } else if (isLocale(split[index]) && split[2] === "blog" && split.length > 2) {
-        split.pop();
+    } else if (isLocale(split[index])) {
         return nav(locale, "/" + split.splice(index + 1).join("/"));
     } else {
         return nav(locale, split.join("/"));
@@ -51,19 +35,6 @@ export const LocaleSwitch = ({
     onClick: () => void;
     setOpened: (open: null | number) => void;
 }) => {
-    /* Suite de la partie commentée mais marche pas
-const pathname = usePathname();
-const pathParts = pathname.split("/");
-const isBlogPost = pathParts.includes("blog");
-const postSlug = isBlogPost ? pathParts[pathParts.length - 1] : null;
-const [post, setPost] = useState<{ slug: string } | null>(null);
-
-useEffect(() => {
-    if (postSlug) {
-        fetchSlug(postSlug).then(post => setPost(post));
-    }
-}, [postSlug]);
-*/
     const localedUrl = useLocaledUrl(locale === "fr" ? "en" : "fr");
 
     return (
