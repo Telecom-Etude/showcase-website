a16bd05fecc08102168cc8dde499f44ab8c1a096
Allan Villars <allan.villars@telecom-paris.fr>
2025-03-19 17:48:32 +0100

    Got rid of useless db

---
+++ prisma/migrations/20250319154731_/migration.sql
@@ -0,0 +1,26 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `slugtr` on the `Post` table. All the data in the column will be lost.
+
+*/
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Post" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "locale" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "validated" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+INSERT INTO "new_Post" ("content", "createdAt", "id", "locale", "slug", "title", "updatedAt", "validated") SELECT "content", "createdAt", "id", "locale", "slug", "title", "updatedAt", "validated" FROM "Post";
+DROP TABLE "Post";
+ALTER TABLE "new_Post" RENAME TO "Post";
+CREATE UNIQUE INDEX "Post_id_key" ON "Post"("id");
+CREATE UNIQUE INDEX "Post_slug_key" ON "Post"("slug");
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
