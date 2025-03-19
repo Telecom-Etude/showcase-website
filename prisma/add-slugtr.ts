cbc77427249bbb9181f7356059675a5ebbb1d459
Allan Villars <allan.villars@telecom-paris.fr>
2025-03-19 17:49:09 +0100

    Modified it to generate the new column and fixed minor issues

--- prisma/add-slugtr.ts
+++ prisma/add-slugtr.ts
@@ -3,44 +3,63 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function updateSlugTr() {
-    await prisma.post.updateMany({
+    try {
+        // Ajoute la colonne "slugtr" si elle n'existe pas déjà
+        await prisma.$executeRawUnsafe(`
+        ALTER TABLE Post ADD COLUMN slugtr TEXT;
+      `);
+
+        console.log('La colonne "slugtr" a été ajoutée');
+    } catch (error) {
+        console.error("Erreur à l'ajout de la colonne", error);
+    }
+
+    var posts = await prisma.post.findMany({
         where: {
             locale: "fr",
         },
-        data: {
-            slugtr: {
-                set: await prisma.$queryRaw`CONCAT(title, '_not_translated')`,
-            },
-        },
     });
 
-    await prisma.post.updateMany({
+    for (const post of posts) {
+        await prisma.post.update({
+            where: { id: post.id },
+            data: {
+                slugtr: post.title + "_pas_traduit",
+            },
+        });
+    }
+
+    posts = await prisma.post.findMany({
         where: {
             locale: "en",
         },
-        data: {
-            slugtr: {
-                set: await prisma.$queryRaw`CONCAT(title, '_not_translated')`,
-            },
-        },
     });
+
+    for (const post of posts) {
+        await prisma.post.update({
+            where: { id: post.id },
+            data: {
+                slugtr: post.title + "_not_translated",
+            },
+        });
+    }
     // Cette partie ne devrait pas être incluse si les posts ont une version en français et en anglais car ces derniers seraient dupliqués.
-    const posts = await prisma.post.findMany({
+    const posts_to_dupe = await prisma.post.findMany({
         include: {
             authors: true,
             labels: true,
         },
     });
 
-    for (const post of posts) {
+    for (const post of posts_to_dupe) {
         const newLocale = post.locale === "fr" ? "en" : "fr";
 
         await prisma.post.create({
             data: {
                 title: post.title + "_not_translated",
                 locale: newLocale,
-                slugtr: post.slugtr,
-                slug: post.slug,
+                slug: post.slugtr === null ? "undefined" : post.slugtr,
+                slugtr: post.slug,
                 authors: {
                     connect: post.authors.map(author => ({ id: author.id })),
                 },
@@ -53,7 +72,6 @@ async function updateSlugTr() {
         });
     }
 }
-
 updateSlugTr()
     .then(() => {
         console.log("Mise à jour terminée");
