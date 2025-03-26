121c6c0afba0887701242c386d9dc585a85c7866
Allan Villars <allan.villars@telecom-paris.fr>
2025-03-26 11:31:03 +0100

    Used constants

--- src/app/api/create-blog/route.ts
+++ src/app/api/create-blog/route.ts
@@ -7,8 +7,9 @@ export async function POST(request: Request) {
         const { authorEmail, title, locale } = await request.json();
         const session = await auth();
         if (session?.user.email !== authorEmail) throw new Error("Email mismatch: Not allowed");
-        const blogId = await createBlog(authorEmail, title, locale, title + "_not_translated");
-        const blogIdTr = await createBlog(authorEmail, title + "_not_translated", locale === "fr" ? "en" : "fr", title);
+        const titletr = locale === "fr" ? title + "_pas_traduit" : title + "_not_translated";
+        const blogId = await createBlog(authorEmail, title, locale, titletr);
+        const blogIdTr = await createBlog(authorEmail, titletr, locale === "fr" ? "en" : "fr", title);
         return NextResponse.json({ blogId });
     } catch (error) {
         console.error("[API Error] Failed to create blog:", error);
