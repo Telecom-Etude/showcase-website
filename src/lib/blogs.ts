ae4a1cc866d71309ed6b84a1f76eb588e3d9d606
Tom Webber <t.c.w.webber@gmail.com>
2025-07-02 14:44:35 +0200

    feat(post-creation): add message for each status to inform the user of the error that occurred

--- src/lib/blogs.ts
+++ src/lib/blogs.ts
@@ -4,3 +4,18 @@ export enum BlogCreationStatus {
     Unauthorised,
     UnknownProblem,
 }
+
+export function blogCreationStatusMessage(status: BlogCreationStatus): string | undefined {
+    switch (status) {
+        case BlogCreationStatus.Ok:
+            return;
+        case BlogCreationStatus.SameTitle:
+            return 'Un blog avec le même nom existe déjà...';
+        // Unauthorised is risen when the API is called manually.
+        // Else, it is not mean't to happen, as the caller should always have the rights on the post.
+        case BlogCreationStatus.Unauthorised:
+            return "Vous n'avez pas les droits pour créer un blog. Merci de contacter le pôle info!";
+        case BlogCreationStatus.UnknownProblem:
+            return "Une erreur s'est produite lors de la création du blog... Réessayez plus tard ou contactez le pôle info!";
+    }
+}
